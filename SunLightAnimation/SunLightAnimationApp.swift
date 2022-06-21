da5e8d9b009ac3a53586f6f6e649c1009f4908b9
AreyBRSK <areybrsk@gmail.com>
2022-06-21 10:42:44 +0300

    Initial Commit

---
+++ SunLightAnimation/SunLightAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunLightAnimationApp.swift
+//  SunLightAnimation
+//
+//  Created by Андрей Барсук on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SunLightAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

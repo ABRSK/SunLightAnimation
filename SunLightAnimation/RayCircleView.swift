24fb79c7efbbadbace46b5d9eb14a3b36e29d038
AreyBRSK <areybrsk@gmail.com>
2022-06-21 11:31:04 +0300

    Added view for inner rays

---
+++ SunLightAnimation/RayCircleView.swift
@@ -0,0 +1,50 @@
+//
+//  RayCircleView.swift
+//  SunLightAnimation
+//
+//  Created by Андрей Барсук on 21.06.2022.
+//
+
+import SwiftUI
+
+struct RayCircleView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            let size = min(geometry.size.width, geometry.size.height)
+            let middle = size / 2
+            
+            Path { path in
+                path.addArc(
+                    center: CGPoint(x: middle, y: middle),
+                    radius: middle,
+                    startAngle: .degrees(0),
+                    endAngle: .degrees(180),
+                    clockwise: true
+                )
+                path.addArc(
+                    center: CGPoint(x: middle, y: middle),
+                    radius: middle,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(0),
+                    clockwise: true
+                )
+            }
+            .stroke(.yellow)
+            
+            Path { path in
+                path.move(to: CGPoint(x: middle, y: 0))
+                path.addLine(to: CGPoint(x: middle, y: size))
+                path.move(to: CGPoint(x: 0, y: middle))
+                path.addLine(to: CGPoint(x: size, y: middle))
+            }
+            .stroke(.orange)
+        }
+    }
+}
+
+struct RayCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        RayCircleView()
+            .frame(width: 200, height: 200)
+    }
+}
